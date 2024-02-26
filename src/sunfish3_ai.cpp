13c494e2e9a3f97110cdca9831d0dae7b2d76158
kbinani <kbinani.bt@gmail.com>
2024-02-26 15:34:11 +0900

    fix: sunfish::Move.piece は成る場合成る前の状態を表す (2)

--- src/sunfish3_ai.cpp
+++ src/sunfish3_ai.cpp
@@ -112,7 +112,11 @@ optional<sunfish::Move> SunfishMoveFromMove(Move const &move) {
   sunfish::Square from;
   PieceType type = PieceTypeFromPiece(move.piece);
   Color color = ColorFromPiece(move.piece);
-  sunfish::Piece piece = SunfishPieceFromPiece(MakePiece(color, type));
+  PieceStatus status = IsPromotedPiece(move.piece) ? PieceStatus::Promoted : PieceStatus::Default;
+  if (move.promote == 1) {
+    status = PieceStatus::Default;
+  }
+  sunfish::Piece piece = SunfishPieceFromPiece(MakePiece(color, type, status));
   if (move.from) {
     from = SunfishSquareFromSquare(*move.from);
   } else {
@@ -135,6 +139,82 @@ optional<sunfish::Move> SunfishMoveFromMove(Move const &move) {
 } // namespace
 
 struct Sunfish3AI::Impl {
+  Impl() {
+    random_device seed_gen;
+    engine = make_unique<mt19937_64>(seed_gen());
+    Test();
+  }
+
+  static void Test() {
+    sunfish::Record record;
+    record.init(sunfish::Board::Handicap::Even);
+
+    static auto Make = [](Color color, optional<Square> from, Square to, Piece p, bool promote = false, optional<PieceType> newHand = nullopt) {
+      Move m;
+      m.color = color;
+      m.from = from;
+      m.to = to;
+      m.piece = p;
+      if (promote) {
+        m.promote = 1;
+      }
+      m.newHand = newHand;
+      return m;
+    };
+
+    vector<Move> moves = {
+        // ▲７六歩(77)
+        Make(Color::Black, MakeSquare(File::File7, Rank::Rank7), MakeSquare(File::File7, Rank::Rank6), MakePiece(Color::Black, PieceType::Pawn)),
+        // ▽９四歩(93)
+        Make(Color::White, MakeSquare(File::File9, Rank::Rank3), MakeSquare(File::File9, Rank::Rank4), MakePiece(Color::White, PieceType::Pawn)),
+        // ▲９六歩(97)
+        Make(Color::Black, MakeSquare(File::File9, Rank::Rank7), MakeSquare(File::File9, Rank::Rank6), MakePiece(Color::Black, PieceType::Pawn)),
+        // ▽９三桂(81)
+        Make(Color::White, MakeSquare(File::File8, Rank::Rank1), MakeSquare(File::File9, Rank::Rank3), MakePiece(Color::White, PieceType::Knight)),
+        // ▲９七角(88)
+        Make(Color::Black, MakeSquare(File::File8, Rank::Rank8), MakeSquare(File::File9, Rank::Rank7), MakePiece(Color::Black, PieceType::Bishop)),
+        // ▽５二飛(82)
+        Make(Color::White, MakeSquare(File::File8, Rank::Rank2), MakeSquare(File::File5, Rank::Rank2), MakePiece(Color::White, PieceType::Rook)),
+        // ▲２六歩(27)
+        Make(Color::Black, MakeSquare(File::File2, Rank::Rank7), MakeSquare(File::File2, Rank::Rank6), MakePiece(Color::Black, PieceType::Pawn)),
+        // ▽４二玉(51)
+        Make(Color::White, MakeSquare(File::File5, Rank::Rank1), MakeSquare(File::File4, Rank::Rank2), MakePiece(Color::White, PieceType::King)),
+        // ▲２五歩(26)
+        Make(Color::Black, MakeSquare(File::File2, Rank::Rank6), MakeSquare(File::File2, Rank::Rank5), MakePiece(Color::Black, PieceType::Pawn)),
+        // ▽８二飛(52)
+        Make(Color::White, MakeSquare(File::File5, Rank::Rank2), MakeSquare(File::File8, Rank::Rank2), MakePiece(Color::White, PieceType::Rook)),
+        // ▲２四歩(25)
+        Make(Color::Black, MakeSquare(File::File2, Rank::Rank5), MakeSquare(File::File2, Rank::Rank4), MakePiece(Color::Black, PieceType::Pawn)),
+        // ▽３四歩(33)
+        Make(Color::White, MakeSquare(File::File3, Rank::Rank3), MakeSquare(File::File3, Rank::Rank4), MakePiece(Color::White, PieceType::Pawn)),
+        // ▲８八銀(79)
+        Make(Color::Black, MakeSquare(File::File7, Rank::Rank9), MakeSquare(File::File8, Rank::Rank8), MakePiece(Color::Black, PieceType::Silver)),
+        // ▽２四歩(23)
+        Make(Color::White, MakeSquare(File::File2, Rank::Rank3), MakeSquare(File::File2, Rank::Rank4), MakePiece(Color::White, PieceType::Pawn), false, PieceType::Pawn),
+        // ▲同飛(28)
+        Make(Color::Black, MakeSquare(File::File2, Rank::Rank8), MakeSquare(File::File2, Rank::Rank4), MakePiece(Color::Black, PieceType::Rook), false, PieceType::Pawn),
+        // ▽８五桂(93)
+        Make(Color::White, MakeSquare(File::File9, Rank::Rank3), MakeSquare(File::File8, Rank::Rank5), MakePiece(Color::White, PieceType::Knight)),
+        // ▲８六角(97)
+        Make(Color::Black, MakeSquare(File::File9, Rank::Rank7), MakeSquare(File::File8, Rank::Rank6), MakePiece(Color::Black, PieceType::Bishop)),
+        // ▽８八角成(22)
+        Make(Color::White, MakeSquare(File::File2, Rank::Rank2), MakeSquare(File::File8, Rank::Rank8), MakePiece(Color::White, PieceType::Bishop), true, PieceType::Silver),
+        // ▲２二歩打
+        Make(Color::Black, nullopt, MakeSquare(File::File2, Rank::Rank2), MakePiece(Color::Black, PieceType::Pawn)),
+        // ▽同馬(88)
+        Make(Color::White, MakeSquare(File::File8, Rank::Rank8), MakeSquare(File::File2, Rank::Rank2), MakePiece(Color::White, PieceType::Bishop, PieceStatus::Promoted), false, PieceType::Pawn),
+        // ▲３四飛(24)
+        Make(Color::Black, MakeSquare(File::File2, Rank::Rank4), MakeSquare(File::File3, Rank::Rank4), MakePiece(Color::Black, PieceType::Rook), false, PieceType::Pawn),
+    };
+    for (int i = 0; i < moves.size(); i++) {
+      Move move = moves[i];
+      auto mv = SunfishMoveFromMove(move);
+      if (!record.makeMove(*mv)) {
+        cout << "ng" << endl;
+      }
+    }
+  }
+
   optional<Move> next(Position const &p, vector<Move> const &moves, deque<PieceType> const &hand, deque<PieceType> const &handEnemy) {
     Color color = moves.size() % 2 == 0 ? Color::Black : Color::White;
     sunfish::Move move = sunfish::Move::empty();
