228bdc9ff677fb72be9803827aacebf09d6092c6
kbinani <kbinani.bt@gmail.com>
2024-02-29 15:16:42 +0900

    feat: キャプチャ画像を表示する

---
+++ app/CGRect+Utility.swift
@@ -0,0 +1,60 @@
+import CoreGraphics
+import UIKit
+
+extension CGRect {
+  mutating func expand(_ dx: CGFloat, _ dy: CGFloat) {
+    let width = max(0, self.width + dx * 2)
+    let height = max(0, self.height + dy * 2)
+    let cx = self.midX
+    let cy = self.midY
+    self.origin = .init(x: cx - width * 0.5, y: cy - height * 0.5)
+    self.size = .init(width: width, height: height)
+  }
+
+  mutating func reduce(_ edgeInsets: UIEdgeInsets) {
+    let width = max(0, self.width - edgeInsets.left - edgeInsets.right)
+    let height = max(0, self.height - edgeInsets.top - edgeInsets.bottom)
+    let cx = self.midX
+    let cy = self.midY
+    let left = min(cx, self.minX + edgeInsets.left)
+    let right = max(cx, self.maxX - edgeInsets.right)
+    let top = min(cy, self.minY + edgeInsets.top)
+    let bottom = max(cy, self.maxY - edgeInsets.bottom)
+    self.origin = .init(x: left, y: top)
+    self.size = .init(width: right - left, height: bottom - top)
+  }
+
+  @discardableResult
+  mutating func removeFromTop(_ amount: CGFloat) -> CGRect {
+    let r = CGRect(x: self.minX, y: self.minY, width: self.width, height: min(amount, self.height))
+    self.origin = .init(x: self.minX, y: self.minY + r.height)
+    self.size = .init(width: self.width, height: self.height - r.height)
+    return r
+  }
+
+  @discardableResult
+  mutating func removeFromLeft(_ amount: CGFloat) -> CGRect {
+    let r = CGRect(x: self.minX, y: self.minY, width: min(amount, self.width), height: self.height)
+    self.origin = .init(x: self.minX + r.width, y: self.minY)
+    self.size = .init(width: self.width - r.width, height: self.height)
+    return r
+  }
+
+  @discardableResult
+  mutating func removeFromRight(_ amount: CGFloat) -> CGRect {
+    let remove = min(amount, self.width)
+    let r = CGRect(
+      x: self.minX + self.width - remove, y: self.minY, width: remove, height: self.height)
+    self.size = .init(width: self.width - remove, height: self.height)
+    return r
+  }
+
+  @discardableResult
+  mutating func removeFromBottom(_ amount: CGFloat) -> CGRect {
+    let remove = min(amount, self.height)
+    let r = CGRect(
+      x: self.minX, y: self.minY + self.height - remove, width: self.width, height: remove)
+    self.size = .init(width: self.width, height: self.height - remove)
+    return r
+  }
+}
--- app/MainViewController.swift
+++ app/MainViewController.swift
@@ -1,20 +1,53 @@
+import AVFoundation
 import UIKit
 
 class MainViewController: UIViewController {
   private var startAsBlackButton: UIButton!
   private var startAsWhiteButton: UIButton!
+  private let session: AVCaptureSession?
+  private var videoView: UIView!
+  private var previewLayer: AVCaptureVideoPreviewLayer?
+
+  override init(nibName: String?, bundle: Bundle?) {
+    self.session = Self.CreateCaptureSession()
+    super.init(nibName: nibName, bundle: bundle)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     self.view.backgroundColor = .darkGray
+    print(self.view.window?.safeAreaInsets)
+
+    let videoView = UIView(frame: .zero)
+    self.view.addSubview(videoView)
+    self.videoView = videoView
+
+    if let session {
+      let preview = AVCaptureVideoPreviewLayer(session: session)
+      preview.videoGravity = .resizeAspect
+      if #available(iOS 17, *) {
+        preview.connection?.videoRotationAngle = 90
+      } else {
+        preview.connection?.videoOrientation = .portrait
+      }
+      videoView.layer.addSublayer(preview)
+      self.previewLayer = preview
+    } else {
+
+    }
 
     let startAsBlackButton = styleButton(UIButton(type: .custom))
     startAsBlackButton.setTitle("先手でスタート", for: .normal)
     startAsBlackButton.setTitleColor(.white, for: .normal)
     startAsBlackButton.setTitleColor(.black, for: .highlighted)
-    startAsBlackButton.backgroundColor = .black
+    startAsBlackButton.backgroundColor = .gray
     startAsBlackButton.tintColor = .white
+    startAsBlackButton.isEnabled = false
     startAsBlackButton.addTarget(
       self, action: #selector(startAsBlackButtonDidTouchUpInside(_:)), for: .touchUpInside)
     startAsBlackButton.addTarget(
@@ -26,18 +59,24 @@ class MainViewController: UIViewController {
     startAsWhiteButton.setTitle("後手でスタート", for: .normal)
     startAsWhiteButton.setTitleColor(.black, for: .normal)
     startAsWhiteButton.setTitleColor(.white, for: .highlighted)
-    startAsWhiteButton.backgroundColor = .white
+    startAsWhiteButton.backgroundColor = .gray
+    startAsWhiteButton.isEnabled = false
     startAsWhiteButton.addTarget(
       self, action: #selector(startAsWhiteButtonDidTouchUpInside(_:)), for: .touchUpInside)
     startAsWhiteButton.addTarget(
       self, action: #selector(startAsWhiteButtonDidTouchDown(_:)), for: .touchDown)
     self.view.addSubview(startAsWhiteButton)
     self.startAsWhiteButton = startAsWhiteButton
+
+    DispatchQueue.global().async { [weak session] in
+      session?.startRunning()
+    }
   }
 
   private func styleButton(_ button: UIButton) -> UIButton {
     button.titleLabel?.textAlignment = .center
     button.layer.cornerRadius = 15
+    button.setTitleColor(.lightGray, for: .disabled)
     return button
   }
 
@@ -45,13 +84,20 @@ class MainViewController: UIViewController {
     super.viewDidLayoutSubviews()
     let size = self.view.bounds.size
     let margin: CGFloat = 15
-    let buttonWidth = (size.width - 3 * margin) / 2
-    let buttonHeight: CGFloat = 90
-    self.startAsBlackButton.frame = .init(
-      x: margin, y: size.height * 0.5, width: buttonWidth, height: buttonHeight)
-    self.startAsWhiteButton.frame = .init(
-      x: size.width * 0.5 + margin * 0.5, y: size.height * 0.5, width: buttonWidth,
-      height: buttonHeight)
+
+    var bounds = CGRect(origin: .zero, size: size)
+    bounds.reduce(self.view.safeAreaInsets)
+    bounds.expand(-margin, -margin)
+
+    var video: CGRect = bounds.removeFromTop(size.height / 2)
+    video.removeFromBottom(margin)
+    self.videoView.frame = video
+    self.previewLayer?.frame = .init(origin: .zero, size: video.size)
+
+    var buttons = bounds.removeFromTop(90)
+    let buttonWidth = (buttons.width - margin) / 2
+    self.startAsBlackButton.frame = buttons.removeFromLeft(buttonWidth)
+    self.startAsWhiteButton.frame = buttons.removeFromRight(buttonWidth)
   }
 
   @objc private func startAsBlackButtonDidTouchUpInside(_ sender: UIButton) {
@@ -68,5 +114,51 @@ class MainViewController: UIViewController {
 
   @objc private func startAsWhiteButtonDidTouchDown(_ sender: UIButton) {
     self.startAsWhiteButton.backgroundColor = .gray
+  }
+
+  private static func CreateCaptureSession() -> AVCaptureSession? {
+    guard
+      let device = AVCaptureDevice.devices().first(where: {
+        $0.position == AVCaptureDevice.Position.back
+      })
+    else {
+      return nil
+    }
+    guard let deviceInput = try? AVCaptureDeviceInput(device: device) else {
+      return nil
+    }
+    let videoDataOutput = AVCaptureVideoDataOutput()
+    videoDataOutput.videoSettings = [
+      kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA
+    ]
+    //    videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global())
+    videoDataOutput.alwaysDiscardsLateVideoFrames = true
+    let session = AVCaptureSession()
+    guard session.canAddInput(deviceInput) else {
+      return nil
+    }
+    session.addInput(deviceInput)
+    guard session.canAddOutput(videoDataOutput) else {
+      return nil
+    }
+    session.addOutput(videoDataOutput)
+
+    session.sessionPreset = AVCaptureSession.Preset.medium
+    if let range = device.activeFormat.videoSupportedFrameRateRanges.max(by: { left, right in
+      left.maxFrameRate < right.maxFrameRate
+    }) {
+      do {
+        try device.lockForConfiguration()
+        defer {
+          device.unlockForConfiguration()
+        }
+        let rate = min(max(5, range.minFrameRate), range.maxFrameRate)
+        device.activeVideoMinFrameDuration = CMTime(
+          seconds: 1 / rate, preferredTimescale: 1_000_000)
+      } catch {
+        print(error)
+      }
+    }
+    return session
   }
 }
