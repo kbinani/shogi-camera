f90861d33da83f43982b4d94c673b7a996c3a613
kbinani <kbinani.bt@gmail.com>
2024-03-03 20:16:47 +0900

    deps: simulator 用の framework はビルドしない

--- deps/opencv.sh
+++ deps/opencv.sh
@@ -10,6 +10,6 @@ set -ue
   unzip "${OPENCV_VERSION}.zip"
   ln -sf opencv-${OPENCV_VERSION} opencv
   cd "opencv-${OPENCV_VERSION}"
-  python ./platforms/apple/build_xcframework.py --out ./build-ios --iphoneos_archs arm64 --iphonesimulator_archs x86_64,arm64 --iphoneos_deployment_target 15 --build_only_specified_archs --without videoio --without video --without ts --without stitching --without photo --without ml --without gapi
+  python ./platforms/apple/build_xcframework.py --out ./build-ios --iphoneos_archs arm64 --iphoneos_deployment_target 15 --build_only_specified_archs --without videoio --without video --without ts --without stitching --without photo --without ml --without gapi
   python ./platforms/osx/build_framework.py --out ./build-macos --without videoio --without video --without ts --without stitching --without photo --without ml --without gapi
 )
