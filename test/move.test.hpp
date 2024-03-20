869e5213cf5da18e0404b33da1e59fcadfa10950
kbinani <kbinani.bt@gmail.com>
2024-03-20 17:51:36 +0900

    cleanup: #pragma once を追加

--- test/move.test.hpp
+++ test/move.test.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 static Position EmptyPosition() {
   Position p;
   for (int y = 0; y < 9; y++) {
--- test/sunfish3.test.hpp
+++ test/sunfish3.test.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 TEST_CASE("Sunfish3") {
   Sunfish3AI::RunTests();
 }
