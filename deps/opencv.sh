58c25e7a523b0d38273d0d3e549b90d99bc28cdd
kbinani <kbinani.bt@gmail.com>
2024-04-07 20:19:10 +0900

    chore: 使っていない imgcodec を除外してビルド

--- deps/opencv.sh
+++ deps/opencv.sh
@@ -10,7 +10,8 @@ set -ue
   unzip "${OPENCV_VERSION}.zip"
   ln -sf opencv-${OPENCV_VERSION} opencv
   cd "opencv-${OPENCV_VERSION}"
-  DISABLE="--without calib3d --without dnn --without gapi --without java --without js --without ml --without objdetect --without photo --without python --without stitching --without ts --without video --without videoio"
-  python ./platforms/apple/build_xcframework.py --out ./build-ios --iphoneos_archs arm64 --iphoneos_deployment_target 15 --build_only_specified_archs $DISABLE
-  python ./platforms/osx/build_framework.py --out ./build-macos $DISABLE
+  WITHOUT_FLAGS="--without calib3d --without dnn --without gapi --without java --without js --without ml --without objdetect --without photo --without python --without stitching --without ts --without video --without videoio"
+  DISABLE_FLAGS="--disable JPEG --disable WEBP --disable SPNG --disable IMGCODEC_HDR --disable IMGCODEC_SUNRASTER --disable IMGCODEC_PXM --disable IMGCODEC_PFM"
+  python ./platforms/apple/build_xcframework.py --out ./build-ios --disable-bitcode --iphoneos_archs arm64 --iphoneos_deployment_target 15 --build_only_specified_archs $WITHOUT_FLAGS $DISABLE_FLAGS
+  python ./platforms/osx/build_framework.py --out ./build-macos $WITHOUT_FLAGS $DISABLE_FLAGS
 )
